e82da033007e44165cd7f97565e8c13a8d74cb13
womoak75 <112880996+womoak75@users.noreply.github.com>
2024-10-01 21:30:53 +0200

    add flag PDBUG enable debugging statements -> compile fitting binary image

--- lib/plugins/src/base/PluginDebug.h
+++ lib/plugins/src/base/PluginDebug.h
@@ -9,11 +9,15 @@ enum PDebugLevel { NONE, WARN, INFO, DEBUG, TRACE };
 class PDebugClass {
     public:
     PDebugClass() {}
+#ifdef PDEBUG
     template<typename... Args> size_t printf(PDebugLevel level, const char * f, Args... args) {
         if(print != nullptr && isLevel(level))
            return print->printf(f, args...);
         return 0;
     }   
+#else
+    template<typename... Args> size_t printf(PDebugLevel level, const char * f, Args... args) {}
+#endif
 
     void setPrint(Print* p) {
         print = p;
